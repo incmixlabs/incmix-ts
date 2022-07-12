02d9d6fbddcbbd729d12bf7dc1197a477f6e1e08
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2022-07-12 11:30:39 -0700

    add support for interfaces

---
+++ packages/ts.runtime/src/lib/visit/visitInterfaceDeclaration.ts
@@ -0,0 +1,56 @@
+import ts from "typescript";
+import { Visiter } from "../helpers/types";
+import { visit } from "./visit";
+
+export const visitInterfaceDeclaration: Visiter<ts.ClassLikeDeclaration> = (
+  node,
+  metadata
+) => {
+  const members = node.members;
+  const typeParameters = node.typeParameters;
+  return ts.factory.createVariableStatement(
+    [ts.factory.createModifier(ts.SyntaxKind.ExportKeyword)],
+    ts.factory.createVariableDeclarationList(
+      [
+        ts.factory.createVariableDeclaration(
+          ts.factory.createIdentifier(node.name!.text + `_$TSR`),
+          undefined,
+          undefined,
+          ts.factory.createObjectLiteralExpression(
+            [
+              ...(metadata ?? []),
+              ts.factory.createPropertyAssignment(
+                "type",
+                ts.factory.createStringLiteral("interface")
+              ),
+              ts.factory.createPropertyAssignment(
+                "members",
+                ts.factory.createObjectLiteralExpression(
+                  members.map(
+                    (member) => visit(member) as ts.PropertyAssignment
+                  ),
+                  true
+                )
+              ),
+              typeParameters
+                ? ts.factory.createPropertyAssignment(
+                    "generics",
+                    ts.factory.createArrayLiteralExpression(
+                      typeParameters?.map(
+                        (node) => visit(node) as ts.Expression
+                      ),
+                      true
+                    )
+                  )
+                : undefined,
+            ]
+              .filter((item) => item)
+              .map((item) => item!),
+            true
+          )
+        ),
+      ],
+      ts.NodeFlags.Const
+    )
+  );
+};
