f836a11f804addd41191c25f926018385a919c83
Alexis Labat <alexislabat@hotmail.com>
2022-07-15 16:46:11 +0100

    Added metadata for id and removed comments

---
+++ .changeset/nervous-lemons-rest.md
@@ -0,0 +1,5 @@
+---
+"@incmix/ts.runtime": minor
+---
+
+Fixed enum id reference and finalised enum visit function
--- packages/ts.runtime/src/lib/visit/visitEnumDeclaration.ts
+++ packages/ts.runtime/src/lib/visit/visitEnumDeclaration.ts
@@ -1,36 +1,8 @@
 import ts from "typescript";
 import { Visiter } from "../helpers/types";
-import {randomUUID} from "crypto";
-
-// Desired output:
-// export enum Enum {
-//     A,
-//     B = 2,
-//     C = B * 2,
-//     D = "12".length
-// }
-//
-// export const Enum_$TSR = {
-//     id: "ce3a032b-0309-48fd-b32a-dda0a47db2c1",
-//     type: "enum",
-//     enum: Enum
-// }
-
-
 
 export const visitEnumDeclaration: Visiter<ts.EnumDeclaration> = (node, metadata) => {
     const {name} = node;
-
-    // TODO both of these are required at runtime -
-    //  * the {type: "enum"...} is required to show that an enum is being used
-    //  * and the actual enum is required so that the reference to the symbol in the above object can be resolved
-
-    // return ts.factory.createEnumDeclaration(
-    //     undefined,
-    //     [ts.factory.createModifier(ts.SyntaxKind.ExportKeyword)],
-    //     ts.factory.createIdentifier("Enum"),
-    //     node.members
-    // );
 
     return ts.factory.createVariableStatement(
         [ts.factory.createModifier(ts.SyntaxKind.ExportKeyword)],
@@ -41,10 +13,7 @@ export const visitEnumDeclaration: Visiter<ts.EnumDeclaration> = (node, metadata
                 undefined,
                 ts.factory.createObjectLiteralExpression(
                     [
-                        ts.factory.createPropertyAssignment(
-                            ts.factory.createIdentifier("id"),
-                            ts.factory.createStringLiteral(randomUUID())
-                        ),
+                        ...metadata ?? [],
                         ts.factory.createPropertyAssignment(
                             ts.factory.createIdentifier("type"),
                             ts.factory.createStringLiteral("enum")
