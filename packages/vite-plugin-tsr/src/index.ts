b3174c7ac8aed044adc2453766a24ed698dac41b
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2022-07-20 14:17:27 -0700

    get ts.runtime working

--- packages/vite-plugin-tsr/src/index.ts
+++ packages/vite-plugin-tsr/src/index.ts
@@ -1,15 +1,22 @@
 import { Plugin } from "vite";
 
-const {
-  transform,
-} = require("@incmix/ts.runtime");
+import { transform as tsTransform } from "@incmix/ts.runtime";
+import { Failable, id as tsrId } from "@incmix/ts.runtime";
 
 export function viteTsrPlugin(): Plugin {
   return {
     name: "vite-plugin-tsr",
     enforce: "pre",
     async transform(code, id, options) {
-      return { code: transform(code, id, options) };
+      return {
+        code: Failable.runFailure<string, Failable.Success<string>>(
+          tsTransform(
+            { text: code, filename: id, outputFilename: `${id}.ts` },
+            { id: tsrId }
+          ),
+          Failable.unwrapFailure
+        ).value,
+      };
     },
   };
 }
@@ -17,6 +24,11 @@ export function viteTsrPlugin(): Plugin {
 function generateTsRuntimeFiles(code: string, id = "", options) {
   if (id.endsWith(".tsr")) {
     // call ts.runtime API here to generate TS Runtime object from the source.
-    return transform(code);
+    return tsTransform(
+      { text: code, filename: id, outputFilename: `${id}.ts` },
+      {
+        id: tsrId,
+      }
+    );
   }
 }
