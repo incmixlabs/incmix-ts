b882c4258997e17b5239323e8e5451e5841474df
Alexis Labat <alexislabat@hotmail.com>
2022-08-01 18:47:14 +0100

    Added getType which allows for type references or types to be resolved to their corresponding type

--- packages/ts.runtime/src/lib/helpers/getType.ts
+++ packages/ts.runtime/src/lib/helpers/getType.ts
@@ -1,7 +1,6 @@
 import ts from "typescript";
 import {checker} from "../../transform";
 
-
 export const getType = <T extends ts.Type>(node: T | ts.TypeReferenceNode) =>
     ((ts.isTypeReferenceNode(node as ts.Node) ?
         checker.getTypeAtLocation(node as ts.TypeReferenceNode) : node
