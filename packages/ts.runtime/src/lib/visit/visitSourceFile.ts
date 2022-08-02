12f6ff807a8317c089156aa92c63ec8f4340ca63
Alexis Labat <alexislabat@hotmail.com>
2022-08-02 16:56:27 +0100

    Types that don't have the export modifier are now not exported in the output file. And added an exclusions list to block other statements

--- packages/ts.runtime/src/lib/visit/visitSourceFile.ts
+++ packages/ts.runtime/src/lib/visit/visitSourceFile.ts
@@ -3,9 +3,24 @@ import { mapNodeChildren } from "../helpers/mapNodeChildren";
 import { Visiter } from "../helpers/types";
 import { visit } from "./visit";
 
+function includeNode(node: ts.Node) {
+  const exclusionList: readonly ts.SyntaxKind[] = [
+      // TODO populate this list with the nodes to exclude
+  ];
+  const isAnExportedType =
+      ts.isTypeAliasDeclaration(node) &&
+      !!(node as ts.TypeAliasDeclaration).modifiers &&
+      !!(node as ts.TypeAliasDeclaration).modifiers!
+          .find(modifier => modifier.kind === ts.SyntaxKind.ExportKeyword);
+
+  return isAnExportedType && !exclusionList.includes(node.kind);
+}
+
 export const visitSourceFile: Visiter<ts.SourceFile> = ({ node, deps }) => {
   return ts.factory.createSourceFile(
-    node.statements.map((s) => visit({ deps, node: s }) as Statement),
+    node.statements
+        .filter(includeNode)
+        .map((s) => visit({ deps, node: s }) as Statement),
     node.endOfFileToken,
     node.flags
   );
