d12af29237c65b0b4228e207534fe64509c4cf44
Alexis Labat <alexislabat@hotmail.com>
2022-08-23 11:56:54 +0100

    test(cli): added folder compilation test

--- packages/cli/__tests__/cli.test.ts
+++ packages/cli/__tests__/cli.test.ts
@@ -1,5 +1,18 @@
-import { Failable, FileOutput, Id } from "@ts-r/core";
-import { setupTestDir } from "@ts-r/core/__tests__/helpers/testFileIO";
+import {
+  Failable,
+  FileOutput,
+  fileOutput,
+  getFullFilePath,
+  Id,
+} from "@ts-r/core";
+import {
+  createTestDir,
+  readTest,
+  setupTestDir,
+  wrapInTestDir,
+  writeTest,
+} from "@ts-r/core/__tests__/helpers/testFileIO";
+import path = require("path");
 
 import { cli } from "../src/cli";
 
@@ -56,6 +69,208 @@ describe(cli, () => {
     expect(logs.includes("Usage: ")).toBeTruthy();
     expect(logs.includes(""));
     expect(exited).toBe(true);
+  });
+
+  it("Should handle folder compilation", () => {
+    type file = {
+      readonly name: string;
+      readonly content: string | readonly file[];
+    };
+    const writeTestDir = (testFile: file, dir?: string) => {
+      if (typeof testFile.content === "string") {
+        // file is not a directory, so write it at the current directory
+        writeTest(
+          wrapInTestDir(`${dir}${path.sep}${testFile.name}`),
+          testFile.content
+        );
+      } else {
+        // file is a directory - so create it, walk the kids and append this directory
+        createTestDir(
+          dir === undefined
+            ? testFile.name
+            : `${dir}${path.sep}${testFile.name}`
+        );
+        testFile.content.forEach((subFile) =>
+          writeTestDir(
+            subFile,
+            dir === undefined
+              ? testFile.name
+              : `${dir}${path.sep}${testFile.name}`
+          )
+        );
+      }
+    };
+    const compareTestDir = (expectedFile: file, dir?: string) => {
+      if (typeof expectedFile.content === "string") {
+        // file is not a directory, so compare the actual file's contents
+        // against the expected file's contents
+        expect(
+          readTest(wrapInTestDir(`${dir}${path.sep}${expectedFile.name}`))
+        ).toBe(expectedFile.content);
+      } else {
+        // file is a directory - so walk the kids, append this directory
+        // and validate each child
+        expectedFile.content.forEach((subFile) =>
+          compareTestDir(
+            subFile,
+            dir === undefined
+              ? expectedFile.name
+              : `${dir}${path.sep}${expectedFile.name}`
+          )
+        );
+      }
+    };
+
+    const testDirName = "in_dir";
+
+    const inDir: file = {
+      name: testDirName,
+      content: [
+        {
+          name: "file1.tsr.ts",
+          content: "export type T = {a: string;};",
+        },
+        {
+          name: "file2.tsr.ts",
+          content: "export type U = {c: boolean;};",
+        },
+        {
+          name: "dir1",
+          content: [
+            {
+              name: "dir1file1.tsr.ts",
+              content: "export type S = {b: number;};",
+            },
+            {
+              name: "dir2",
+              content: [
+                {
+                  name: "dir1dir2file1.tsr.ts",
+                  content: "export type K = {l: string;};",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    const outDir: file = {
+      name: testDirName,
+      content: [
+        {
+          name: "file1.tsr.o.ts",
+          content:
+            "export const T_$TSR = {\n" +
+            `    id: "${cliTestID}",\n` +
+            '    type: "object",\n' +
+            "    properties: {\n" +
+            "        a: {\n" +
+            '            type: "propertySignature",\n' +
+            "            optional: false,\n" +
+            `            tsRuntimeObject: { id: "${cliTestID}", type: "string" }\n` +
+            "        }\n" +
+            "    }\n" +
+            "} as const;\n",
+        },
+        {
+          name: "file2.tsr.o.ts",
+          content:
+            "export const U_$TSR = {\n" +
+            `    id: "${cliTestID}",\n` +
+            '    type: "object",\n' +
+            "    properties: {\n" +
+            "        c: {\n" +
+            '            type: "propertySignature",\n' +
+            "            optional: false,\n" +
+            `            tsRuntimeObject: { id: "${cliTestID}", type: "boolean" }\n` +
+            "        }\n" +
+            "    }\n" +
+            "} as const;\n",
+        },
+        {
+          name: "dir1",
+          content: [
+            {
+              name: "dir1file1.tsr.o.ts",
+              content:
+                "export const S_$TSR = {\n" +
+                `    id: "${cliTestID}",\n` +
+                '    type: "object",\n' +
+                "    properties: {\n" +
+                "        b: {\n" +
+                '            type: "propertySignature",\n' +
+                "            optional: false,\n" +
+                `            tsRuntimeObject: { id: "${cliTestID}", type: "number" }\n` +
+                "        }\n" +
+                "    }\n" +
+                "} as const;\n",
+            },
+            {
+              name: "dir2",
+              content: [
+                {
+                  name: "dir1dir2file1.tsr.o.ts",
+                  content:
+                    "export const K_$TSR = {\n" +
+                    `    id: "${cliTestID}",\n` +
+                    '    type: "object",\n' +
+                    "    properties: {\n" +
+                    "        l: {\n" +
+                    '            type: "propertySignature",\n' +
+                    "            optional: false,\n" +
+                    `            tsRuntimeObject: { id: "${cliTestID}", type: "string" }\n` +
+                    "        }\n" +
+                    "    }\n" +
+                    "} as const;\n",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    // Write inDir test files
+    writeTestDir(inDir);
+
+    // Run CLI on inDir
+    let logs = "";
+    let errors = "";
+    let exited = false;
+
+    cli({
+      deps: {
+        commanderProgram: {
+          exitOverride() {
+            exited = true;
+          },
+        },
+        fileOutput: fileOutput,
+        args: {
+          getArgs() {
+            return [getFullFilePath(wrapInTestDir(testDirName))];
+          },
+          startsOnActualArguments: true,
+        },
+        logger: {
+          log(text) {
+            logs += `${text}\n`;
+          },
+          error(text) {
+            errors += `${text}\n`;
+          },
+        },
+        id: testIdGenerator,
+      },
+    });
+
+    // Ensure that the cli ran without error and didn't exit prematurely
+    expect(errors).toBe("");
+    expect(logs).toBe("");
+    expect(exited).toBe(false);
+
+    // Compare outDir with
+    compareTestDir(outDir);
   });
 
   test.todo("ADD MANY MORE TESTS");
--- packages/core/__tests__/helpers/testFileIO.ts
+++ packages/core/__tests__/helpers/testFileIO.ts
@@ -1,17 +1,25 @@
 import fs from "fs";
+import * as path from "path";
 
-import { getFullFilePath, Path, wrap } from "../../src/Path";
+import { getFullFilePath, Path, wrap } from "../../src";
 
 export const TEST_DIR = "TEMP_TEST_DIR";
 
 export const setupTestDir = () => {
   // Create an empty temporary test directory
-  if (fs.existsSync(TEST_DIR)) fs.rmdirSync(TEST_DIR, { recursive: true });
-  fs.mkdirSync(TEST_DIR);
+  if (!fs.existsSync(TEST_DIR)) fs.mkdirSync(TEST_DIR);
+};
+
+export const createTestDir = (subDir: string) => {
+  subDir = `${TEST_DIR}${path.sep}${subDir}`;
+  if (!fs.existsSync(subDir)) fs.mkdirSync(subDir);
 };
 
 export const writeTest = (filePath: Path, input: string) =>
   fs.writeFileSync(getFullFilePath(filePath), input);
+
+export const readTest = (filePath: Path) =>
+  fs.readFileSync(getFullFilePath(filePath)).toString();
 
 export const wrapInTestDir = (filename: string) => wrap(filename, TEST_DIR);
 
